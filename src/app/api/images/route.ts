06691402206a18970372f2ee5d817f1ecca6a98a
Ben <btorndorff@gmail.com>
2024-05-27 17:46:36 -0700

    fix env

--- src/app/api/images/route.ts
+++ src/app/api/images/route.ts
@@ -2,16 +2,16 @@ import { NextResponse } from "next/server";
 import cloudinary from "cloudinary";
 
 cloudinary.v2.config({
-  cloud_name: process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME,
-  api_key: process.env.NEXT_PUBLIC_CLOUDINARY_API_KEY,
-  api_secret: process.env.NEXT_PUBLIC_CLOUDINARY_API_SECRET,
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
 export async function GET() {
   try {
     const { resources } = await cloudinary.v2.api.resources({
       type: "upload",
-      prefix: "film-portfolio", // Adjust this to your specific folder
+      prefix: "film-portfolio",
       max_results: 100,
     });
 
